5d10833524c165053a91c785fe542074c806843a
cirm <m.jullinen@gmail.com>
2015-10-08 11:09:59 +0300

    Fixed jshint options.

--- gulp/lint.js
+++ gulp/lint.js
@@ -1,18 +1,15 @@
 (function () {
   'use strict';
 
-  var gulp   = require('gulp')
-    , jshint = require('gulp-jshint')
-    , stylish = require('jshint-stylish')
-    , config  = require('./gulp.config')
-    , JSONpackage = require('../jshintconf')
+  var gulp         = require('gulp')
+    , jshint       = require('gulp-jshint')
+    , stylish      = require('jshint-stylish')
+    , config       = require('./gulp.config')
+    , JSONpackage  = require('./jshint.config.js')
     , jshintConfig = JSONpackage.config
-    , watch  = config.paths.sources.watch();
-
-  jshintConfig.lookup = false;
+    , watch        = config.paths.sources.watch();
 
   gulp.task('lint', function () {
-    console.log(jshintConfig);
     return gulp.src(watch)
       .pipe(jshint(jshintConfig))
       .pipe(jshint.reporter(stylish));
