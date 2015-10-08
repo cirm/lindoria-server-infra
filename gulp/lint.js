2b609be9a5f03df3c0e4eb0242c422291b433c5c
cirm <m.jullinen@gmail.com>
2015-10-08 08:17:46 +0300

    jshint fixes

--- gulp/lint.js
+++ gulp/lint.js
@@ -4,12 +4,17 @@
   var gulp   = require('gulp')
     , jshint = require('gulp-jshint')
     , stylish = require('jshint-stylish')
-    , gConf  = require('./gulp.config')
-    , watch  = gConf.paths.sources.watch();
+    , config  = require('./gulp.config')
+    , jshintconf = require('../')
+    , jshintConfig = jshintconf.jshintrc
+    , watch  = config.paths.sources.watch();
+
+  jshintConfig.lookup = false;
 
   gulp.task('lint', function () {
+    console.log(jshintConfig);
     return gulp.src(watch)
-      .pipe(jshint())
+      .pipe(jshint(jshintConfig))
       .pipe(jshint.reporter(stylish));
   });
 
