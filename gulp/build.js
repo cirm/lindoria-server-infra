a09bc8114ad2ef57ab785affc5c260a17083a5dc
cirm <m.jullinen@gmail.com>
2015-10-11 23:29:11 +0300

    Ok, let's wait for gulp4 and solve this the easy way.

--- gulp/build.js
+++ gulp/build.js
@@ -3,7 +3,7 @@
 
   var gulp = require('gulp');
 
-  gulp.task('build.dev', ['clean.build', 'node.build', 'angular.build', 'templates.build', 'css.min', 'copy.ssl']);
+  gulp.task('build.dev', ['node.build', 'angular.build', 'templates.build', 'css.min', 'copy.ssl']);
   gulp.task('update.dev', ['node.update', 'angular.update', 'templates.build', 'css.min']);
 
 })();
--- gulp/clean.js
+++ gulp/clean.js
@@ -9,27 +9,34 @@
     , build       = config.paths.destinations.build
     , temp        = config.paths.destinations.temp;
 
-  gulp.task('clean.angular.min', function (cb) {
-    clean([angularDest + 'all.min.js'], cb);
+  gulp.task('clean.angular.min', function () {
+    return clean([angularDest + 'all.min.js']);
   });
 
-  gulp.task('clean.css.min', function (cb) {
-    clean([cssDest], cb);
+  gulp.task('clean.css.min', function () {
+    return clean([cssDest]);
   });
 
-  gulp.task('clean.temp', function (cb) {
-    clean([temp], cb);
+  gulp.task('clean.temp', function () {
+    return clean([temp]);
   });
-  gulp.task('clean.build', function (cb) {
-    clean([build], cb)
-  });
-
-  gulp.task('clean.templates', function (cb) {
-    clean([angularDest + 'templates.min.js'], cb)
+  gulp.task('clean.build', function () {
+    return clean([build])
   });
 
-  gulp.task('clean.node', function (cb) {
-    clean(['build/**', '!build/ssl/**', '!build/ssl/**', '!build', '!build/public/**', '!build/node_modules/**'], cb);
+  gulp.task('clean.templates', function () {
+    return clean([angularDest + 'templates.min.js'])
+  });
+
+  gulp.task('clean.node', function () {
+    return clean([
+      'build/**',
+      '!build/ssl/**',
+      '!build/ssl/**',
+      '!build',
+      '!build/public/**',
+      '!build/node_modules/**'
+    ]);
   });
 
   gulp.task('clean.all', ['clean.temp', 'clean.build']);
