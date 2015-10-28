925a813ab38fe943f2fa5ea74ac85c0151638fa4
cirm <m.jullinen@gmail.com>
2015-10-28 17:11:35 +0200

    In the middle of everything part 2.

--- docker/db/provision/schemaChangeLog.sql
+++ docker/db/provision/schemaChangeLog.sql
@@ -6,24 +6,27 @@ CREATE SCHEMA dbv
   AUTHORIZATION geegomoonshine;
 
 CREATE TABLE dbv.schemaChangeLog (
-  version     SERIAL,
+  version     INT         PRIMARY KEY,
   description VARCHAR(60) NOT NULL,
   date        DATE        NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO dbv.schemaChangeLog (description)
+INSERT INTO dbv.schemaChangeLog (version, description)
 VALUES (
-  'Initial install'
+  1,
+  'Initial_install'
 );
 
 CREATE OR REPLACE FUNCTION dbv.log_update(
-  IN i_description VARCHAR(4)
+  IN
+  i_version     INT,
+  i_description VARCHAR(4)
 )
   RETURNS BOOLEAN AS
 $BODY$
 BEGIN
-  INSERT INTO dbv.schemaChangeLog (description)
-  VALUES (i_description);
+  INSERT INTO dbv.schemaChangeLog (version, description)
+  VALUES (i_version, i_description);
   RETURN FOUND;
 END;
 $BODY$
